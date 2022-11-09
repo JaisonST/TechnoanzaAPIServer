b9c5b6d3f5edf83e8808d6ee7a739e892034d271
SHREESHMAN <35427748+SHREESHMAN@users.noreply.github.com>
2022-11-09 21:38:09 +0400

    final users file
    id,email and count work

---
+++ features/users.js
@@ -0,0 +1,35 @@
+function user_functions(app,con){
+    //get user details by id 
+    app.get('/user/id/:id', function(req,res) {
+        con.query(`Select * from users where id = ${req.params.id};`, function (err, result) {
+            if (err) throw err;
+            p=JSON.stringify(result);
+            if (p=="[]"){
+                res.status(404).json({message: "User not found", error: "invalid id"})
+                return
+            }
+            else res.end(p);
+        });
+    })
+
+    //get user details by email
+    app.get('/user/email/:email', function(req,res) {
+        con.query(`Select * from users where email = "${req.params.email}";`, function (err, result) {
+            if (err) throw err;
+            p=JSON.stringify(result);
+            if (p=="[]"){res.status(404).json({message: "User not found", error: "invalid email"})
+            return}
+            else res.end(p);    
+        });
+    })
+
+    //get total registered user count 
+    app.get('/user/count', function(req,res) {
+        con.query(`Select count(id) as count from users;`, function (err, result) {
+            if (err) throw err;
+            res.end(JSON.stringify(result));    
+        });
+    })
+}
+
+module.exports = user_functions
