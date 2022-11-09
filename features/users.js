289253b75b974a96233d4a3ba09966c1717c900e
SHREESHMAN <35427748+SHREESHMAN@users.noreply.github.com>
2022-11-09 21:08:40 +0400

    Update users.js

--- features/users.js
+++ features/users.js
@@ -1,17 +1,39 @@
 function user_functions(app,con){
     //get user details by id 
-    app.get('/user/id', function(req,res) {
-        res.end(JSON.stringify({"error": "unimplimented"}));     
+    app.get('/user/id/:id', function(req,res) {
+        con.query(`Select * from users where id = ${req.params.id};`, function (err, result) {
+            if (err) throw err;
+            p=JSON.stringify(result);
+            if (p=="[]"){res.status(404).json({message: "User not found", error: "invalid id"})
+            return}
+            else res.end(p);
+        });
     })
 
     //get user details by email
-    app.get('/user/email', function(req,res) {
-        res.end(JSON.stringify({"error": "unimplimented"}));     
+    app.get('/user/email/:email', function(req,res) {
+        con.query(`Select * from users where email = "${req.params.email}";`, function (err, result) {
+            if (err) throw err;
+            p=JSON.stringify(result);
+            if (p=="[]"){res.status(404).json({message: "User not found", error: "invalid email"})
+            return}
+            else res.end(p);    
+        });
     })
+
     
     //get total registered user count 
     app.get('/user/count', function(req,res) {
-        res.end(JSON.stringify({"error": "unimplimented"}));     
+        con.query(`select count(distinct id) as count from users;`, function (err, result) {
+            if (err) throw err;
+            p=JSON.stringify(result);
+            //console.log(result[0]['count'])
+            if (p=="[]"){
+                res.status(404).json({message: "User not found", error: "invalid email"})
+                return
+            }
+            else res.end(p);    
+        });
     })
 }
 
