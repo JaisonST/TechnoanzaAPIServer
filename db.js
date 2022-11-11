1891df551524cc081867cd6d1b51e1feb6cace0e
JaisonST <jaisonmanu@gmail.com>
2022-11-11 10:06:45 +0400

    added cors support

--- db.js
+++ db.js
@@ -6,7 +6,9 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    //uncomment below for server only 
+    //socketPath: '/var/run/mysqld/mysqld.sock'
 });
 
 con.connect(function(err) {
